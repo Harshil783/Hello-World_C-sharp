0e748f82baaab7a1e7687bf90da327521916da18
Harshil55 <harshilpandey57@gmail.com>
2020-11-05 12:40:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
